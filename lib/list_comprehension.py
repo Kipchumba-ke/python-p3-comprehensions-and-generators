f8dec74f1a0b00214df888c7551128932726d27a
Kelvin Kipchumba <chogekelvin90@gmail.com>
2025-01-08 18:11:45 +0300

    comprehensions and generator expressions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even = []
+    for i in num_list:
+        if i % 2 == 0:
+            even.append(i)
+    return even
+   #     else:
+    #        return list()
+    #pass
 
 def make_exclamation(sentence_list):
-    pass
+    return [f"{i}!" for i in sentence_list]
